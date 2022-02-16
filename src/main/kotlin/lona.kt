981c2c4c9e86ea8b101deae537d3e250c8156542
chrilona <christabelalona@gmail.com>
2022-02-16 22:01:10 +0300

    kotlin first assignment

---
+++ src/main/kotlin/lona.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var name : String = ("Lona Christabel" + "" +  "32kg" + " age 43" +  "of phoneNumber 0720693432" + " isKenyan")
+    var name2 : String = ("John Doe" + "" +   "33kg" + " age 65" +  "of phoneNumber 0723456289" + "isnotKenyan")
+    var name3 : String = ("Laura Alinda"  +  "35kg" + "age 17"  + " of phoneNumber 0714524399" + "isnotKenyan")
+     var isKenyan  = true
+     var  isnotKenyan : Boolean = false
+    println(name)
+    println(isKenyan)
+    println(name2)
+    println(isnotKenyan)
+    println(name3)
+    println(isnotKenyan)
+
+
+}
